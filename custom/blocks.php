383f57830581b9f1e90a1d970c0d7b4ec5a74e6b
Coulter Peterson <29346967+coulterpeterson@users.noreply.github.com>
2024-02-15 09:35:00 -0500

    Adding support for child theme blocks

--- custom/blocks.php
+++ custom/blocks.php
@@ -51,7 +51,26 @@ function load_acf_blocks() {
 		}
 	}
 }
+function load_child_acf_blocks() {
+	$blocks = get_child_blocks();
+	foreach ( $blocks as $block ) {
+		if ( file_exists( get_stylesheet_directory() . '/blocks/' . $block . '/block.json' ) ) {
+			// Check if Block is already registered
+			$registry = WP_Block_Type_Registry::get_instance();
+			if ( ! $registry->get_registered( 'wicket/' . $block ) ) {
+				register_block_type( get_stylesheet_directory() . '/blocks/' . $block . '/block.json' );
+				if ( file_exists( get_stylesheet_directory() . '/blocks/' . $block . '/style.css' ) ) {
+					wp_register_style( 'block-' . $block, get_stylesheet_directory_uri() . '/blocks/' . $block . '/style.css', array(), filemtime( get_template_directory() . '/blocks/' . $block . '/style.css' ) );
+				}
+				if ( file_exists( get_stylesheet_directory() . '/blocks/' . $block . '/init.php' ) ) {
+					include_once get_stylesheet_directory() . '/blocks/' . $block . '/init.php';
+				}
+			}
+		}
+	}
+}
 add_action( 'init', 'load_acf_blocks', 5 );
+add_action( 'init', 'load_child_acf_blocks', 5 );
 
 /**
  * Load ACF field groups for blocks
@@ -63,7 +82,15 @@ function load_acf_blocks_field_group( $paths ) {
 	}
 	return $paths;
 }
+function load_child_acf_blocks_field_group( $paths ) {
+	$blocks = get_child_blocks();
+	foreach ( $blocks as $block ) {
+		$paths[] = get_stylesheet_directory() . '/blocks/' . $block;
+	}
+	return $paths;
+}
 add_filter( 'acf/settings/load_json', 'load_acf_blocks_field_group' );
+add_filter( 'acf/settings/load_json', 'load_child_acf_blocks_field_group' );
 
 /**
  * Get ACF Blocks
@@ -71,7 +98,14 @@ add_filter( 'acf/settings/load_json', 'load_acf_blocks_field_group' );
 function get_blocks() {
 	$blocks = scandir( get_template_directory() . '/blocks/' );
 	$blocks = array_values( array_diff( $blocks, array( '..', '.', '.DS_Store', '_base-block' ) ) );
+
 	return $blocks;
+}
+function get_child_blocks() {
+	$child_blocks = scandir( get_stylesheet_directory() . '/blocks/' );
+	$child_blocks = array_values( array_diff( $child_blocks, array( '..', '.', '.DS_Store', '_base-block' ) ) );
+
+	return $child_blocks;
 }
 
 /**
