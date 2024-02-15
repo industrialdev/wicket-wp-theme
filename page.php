b2cd8f7c2c170a2cfa7cd044f2be1130c81b14b2
Coulter Peterson <29346967+coulterpeterson@users.noreply.github.com>
2024-02-15 11:51:48 -0500

    Moving basic page breadcrumbs and published date into the virtual container

--- page.php
+++ page.php
@@ -16,12 +16,12 @@
 
 		<?php
 		if ( $display_breadcrumb ) {
-			echo '<div class="container">';
+			echo '<div class="wp-block-breadcrumbs">'; // Having the `wp-block-` prefix will help align it with the other Blocks
 			get_component( 'breadcrumbs', [] );
 			echo '</div>';
 		}
 		if ( $display_publish_date ) {
-			echo '<div class="container">';
+			echo '<div class="wp-block-published-date">';
 			echo "<p class='mt-3 mb-4'><strong>" . __('Published:', 'wicket') . ' ' . get_the_date( 'd-m-Y' ) . "</strong></p>";
 			echo '</div>';
 		}
