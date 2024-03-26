1ae4bb338238ac0a3fe3e33add5e710a6fc05e9c
aykan <aykanburcak@gmail.com>
2024-03-26 12:47:02 +0300

    Featured posts block fixes

--- components/featured-posts.php
+++ components/featured-posts.php
@@ -22,6 +22,10 @@ $hide_content_type   = $args['hide_content_type'];
 $style               = $args['style'];
 $column_count        = $args['column_count'];
 $classes[]           = 'component-featured-posts';
+
+if ( empty ( $posts ) ) {
+	return;
+}
 ?>
 
 <div class="<?php echo implode( ' ', $classes ) ?>">
@@ -36,11 +40,12 @@ $classes[]           = 'component-featured-posts';
 		<div class="flex flex-col gap-10 lg:flex-row lg:gap-4">
 			<div class="flex-1 lg:basis-5/12">
 				<?php
-				$post         = $posts[0];
-				$post_id      = $post->ID;
-				$image        = [];
-				$content_type = get_the_terms( $post_id, 'content_type' );
-				$post_date    = get_the_date( 'F j, Y', $post_id );
+				$post                 = $posts[0];
+				$post_id              = $post->ID;
+				$image                = [];
+				$related_content_type = get_related_content_type( get_post_type( $post_id ) );
+				$content_type         = ! is_wp_error( get_the_terms( $post_id, $related_content_type ) ) ? get_the_terms( $post_id, $related_content_type ) : [];
+				$post_date            = get_the_date( 'F j, Y', $post_id );
 
 				if ( ! $hide_featured_image ) {
 					$featured_image_id  = get_post_thumbnail_id( $post_id );
@@ -65,35 +70,38 @@ $classes[]           = 'component-featured-posts';
 			</div>
 			<div class="flex-1 lg:basis-7/12">
 				<?php
-				$posts = array_slice( $posts, 1 );
-				$index = 0;
-				foreach ( $posts as $post ) {
-					$index++;
-					$post_id      = $post->ID;
-					$post_date    = get_the_date( 'F j, Y', $post_id );
-					$image        = [];
-					$content_type = get_the_terms( $post_id, 'content_type' );
-					if ( ! $hide_featured_image ) {
-						$featured_image_id  = get_post_thumbnail_id( $post_id );
-						$featured_image_alt = get_post_meta( $featured_image_id, '_wp_attachment_image_alt', true );
-						$image              = [ 
-							'id'  => $featured_image_id,
-							'alt' => $featured_image_alt,
-						];
-					}
-					get_component( 'card-featured', [ 
-						'classes'        => [ 'shadow-none' ],
-						'title'          => get_the_title( $post_id ),
-						'image'          => $image,
-						'image_position' => 'right',
-						'content_type'   => ! $hide_content_type ? $content_type[0]->name : '',
-						'date'           => ! $hide_date ? $post_date : '',
-						'member_only'    => is_member_only( $post_id ),
-						'link'           => get_permalink( $post_id ),
-					] );
+				if ( count( $posts ) > 1 ) {
+					$posts = array_slice( $posts, 1 );
+					$index = 0;
+					foreach ( $posts as $post ) {
+						$index++;
+						$post_id              = $post->ID;
+						$post_date            = get_the_date( 'F j, Y', $post_id );
+						$image                = [];
+						$related_content_type = get_related_content_type( get_post_type( $post_id ) );
+						$content_type         = ! is_wp_error( get_the_terms( $post_id, $related_content_type ) ) ? get_the_terms( $post_id, $related_content_type ) : [];
+						if ( ! $hide_featured_image ) {
+							$featured_image_id  = get_post_thumbnail_id( $post_id );
+							$featured_image_alt = get_post_meta( $featured_image_id, '_wp_attachment_image_alt', true );
+							$image              = [ 
+								'id'  => $featured_image_id,
+								'alt' => $featured_image_alt,
+							];
+						}
+						get_component( 'card-featured', [ 
+							'classes'        => [ 'shadow-none' ],
+							'title'          => get_the_title( $post_id ),
+							'image'          => $image,
+							'image_position' => 'right',
+							'content_type'   => ! $hide_content_type ? $content_type[0]->name : '',
+							'date'           => ! $hide_date ? $post_date : '',
+							'member_only'    => is_member_only( $post_id ),
+							'link'           => get_permalink( $post_id ),
+						] );
 
-					if ( $index < count( $posts ) ) {
-						echo '<hr class="border-t border-light-020 my-4">';
+						if ( $index < count( $posts ) ) {
+							echo '<hr class="border-t border-light-020 my-4">';
+						}
 					}
 				}
 				?>
