8deb58fad7f5187302643f40130a3f2a22a1a66f
aykan <aykanburcak@gmail.com>
2024-06-12 18:54:00 +0300

    Adding filter hooks for helper functions

--- custom/helpers.php
+++ custom/helpers.php
@@ -43,7 +43,7 @@ function wicket_get_site_root_url() {
 
 	$site_url = get_site_url();
 
-	if( empty( $site_url ) ) {
+	if ( empty( $site_url ) ) {
 		$site_url = wicket_get_current_url( true );
 	}
 
@@ -63,78 +63,75 @@ function wicket_get_site_root_url() {
 function wicket_get_current_url( $root_only = false ) {
 	// Credit: https://www.javatpoint.com/how-to-get-current-page-url-in-php
 
-	if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') {
-		$url = "https://";  
+	if ( isset( $_SERVER['HTTPS'] ) && $_SERVER['HTTPS'] === 'on' ) {
+		$url = "https://";
 	} else {
-		$url = "http://";   
+		$url = "http://";
 	}
-         
-	// Append the host(domain name, ip) to the URL.   
-	$url .= $_SERVER['HTTP_HOST'];   
 
-	if( $root_only ) {
+	// Append the host(domain name, ip) to the URL.
+	$url .= $_SERVER['HTTP_HOST'];
+
+	if ( $root_only ) {
 		return $url;
 	}
-    
-	// Append the requested resource location to the URL   
-	$url .= $_SERVER['REQUEST_URI'];    
+
+	// Append the requested resource location to the URL
+	$url .= $_SERVER['REQUEST_URI'];
 
 	return $url;
 }
 
 function wicket_get_lang_url( $lang, $url = '' ) {
-	if( empty( $url ) ) {
+	if ( empty( $url ) ) {
 		$url = wicket_get_current_url();
 	}
 
-	if( has_filter( 'wpml_default_language' ) ) {
+	if ( has_filter( 'wpml_default_language' ) ) {
 		// Filter documentation: https://wpml.org/wpml-hook/wpml_default_language/
-		$default_lang = apply_filters('wpml_default_language', NULL );
+		$default_lang = apply_filters( 'wpml_default_language', NULL );
 	} else {
 		$default_lang = 'en';
 	}
-	
+
 	// We want to convert to the default language
-	if( $lang == $default_lang ) {
+	if ( $lang == $default_lang ) {
 		$split_url = explode( '/', $url );
-		if( $default_lang == 'en' ) {
-			if (($key = array_search('fr', $split_url)) !== false) {
-				unset($split_url[$key]);
+		if ( $default_lang == 'en' ) {
+			if ( ( $key = array_search( 'fr', $split_url ) ) !== false ) {
+				unset( $split_url[ $key ] );
 			}
 			$new_url = implode( '/', $split_url );
 			// Todo: add support for more languages
 		} else {
-			if (($key = array_search('en', $split_url)) !== false) {
-				unset($split_url[$key]);
+			if ( ( $key = array_search( 'en', $split_url ) ) !== false ) {
+				unset( $split_url[ $key ] );
 			}
 			$new_url = implode( '/', $split_url );
 		}
-		
+
 		return $new_url;
 	}
 
-	if( has_filter( 'wpml_permalink' ) ) {
+	if ( has_filter( 'wpml_permalink' ) ) {
 		// Filter documentation: https://wpml.org/wpml-hook/wpml_permalink/
-		return apply_filters( 'wpml_permalink', $url , $lang ); 
+		return apply_filters( 'wpml_permalink', $url, $lang );
 	} else {
 		return $url;
 	}
 }
 
 function get_related_content_type( $post_type ) {
-	// Switch case to return the related content type for a given post type
-	switch ( $post_type ) {
-		case 'news':
-			return 'news_type';
-		case 'resources':
-			return 'resource_type';
-		case 'post':
-			return 'category';
-		case 'tribe_events';
-			return 'tribe_events_cat';
-		default:
-			return '';
-	}
+	$default_content_types = array(
+		'news'         => 'news_type',
+		'resources'    => 'resource_type',
+		'post'         => 'category',
+		'tribe_events' => 'tribe_events_cat',
+	);
+
+	$content_types = apply_filters( 'wicket_related_content_types', $default_content_types );
+
+	return isset( $cases[ $post_type ] ) ? $content_types[ $post_type ] : '';
 }
 
 function get_related_content_type_term( $post_id ) {
@@ -149,16 +146,14 @@ function get_related_content_type_term( $post_id ) {
 }
 
 function get_related_topic_type( $post_type ) {
-	switch ( $post_type ) {
-		case 'news':
-			return 'topics';
-		case 'resources':
-			return 'topics';
-		case 'post':
-			return 'post_tag';
-		case 'tribe_events';
-			return 'post_tag';
-		default:
-			return '';
-	}
+	$default_topic_types = array(
+		'news'         => 'topics',
+		'resources'    => 'topics',
+		'post'         => 'post_tag',
+		'tribe_events' => 'post_tag',
+	);
+
+	$topic_types = apply_filters( 'wicket_related_topic_types', $default_topic_types );
+
+	return isset( $topic_types[ $post_type ] ) ? $topic_types[ $post_type ] : '';
 }
