46666772a0c0a0aedae0e31ed9243861101d78b6
Coulter Peterson <29346967+coulterpeterson@users.noreply.github.com>
2024-08-09 16:06:50 -0400

    Overriding how the core topics/tags block displays the tags

---
+++ assets/scripts/post-terms-topics.js
@@ -0,0 +1,11 @@
+// Modifies the tags inside the post term/topics core Block, because we don't want them clickable, just listed
+window.addEventListener('load', function () {
+  if (document.querySelector('body') !== null) {
+    document.querySelectorAll('.wp-block-post-terms a').forEach(oldTag => {
+      let newTag = document.createElement('div');
+      newTag.innerHTML = oldTag.innerHTML;
+
+      oldTag.parentNode.replaceChild(newTag, oldTag);
+    });
+  }
+});
