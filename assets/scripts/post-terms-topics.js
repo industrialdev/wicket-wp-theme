fa469375893d71af9088386299677d9f3986c585
Coulter Peterson <29346967+coulterpeterson@users.noreply.github.com>
2024-08-16 12:42:06 -0400

    Updating post type tag override

--- assets/scripts/post-terms-topics.js
+++ assets/scripts/post-terms-topics.js
@@ -2,7 +2,7 @@
 window.addEventListener('load', function () {
   if (document.querySelector('body') !== null) {
     document.querySelectorAll('.wp-block-post-terms a').forEach(oldTag => {
-      let newTag = document.createElement('div');
+      let newTag = document.createElement('span');
       newTag.innerHTML = oldTag.innerHTML;
 
       oldTag.parentNode.replaceChild(newTag, oldTag);
