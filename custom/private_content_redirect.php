1bc8b3f0c6a341394c7d7535180952869cf74da9
Esteban <esteban@attitude.cl>
2025-01-29 11:05:34 -0300

    fix redirect loop

--- custom/private_content_redirect.php
+++ custom/private_content_redirect.php
@@ -1,36 +1,35 @@
 <?php
 
-/**------------------------------------------------
+// No direct access
+defined('ABSPATH') || exit;
+
+/**
  * For use with wp private content plus plugin
  * Appends the page to the url on redirect for where
  * the user was trying to go
- * filter applied here wp-private-content-plus\classes\class-wppcp-private-posts-pages.php:183
+ *
+ * Filters applied:
+ * - wp-private-content-plus\classes\class-wppcp-private-posts-pages.php:183 (single post)
+ * - wp-private-content-plus global post restriction
+ *
  * The main header login button then looks at this param and uses it if present
- ------------------------------------------------*/
-function private_content_single_redirect( &$url ) {
-  $url .= '?referrer='.$_SERVER['REQUEST_URI'];
-  $lang = 'en';
-  if( defined( 'ICL_LANGUAGE_CODE' ) ) {
-      $lang = ICL_LANGUAGE_CODE;
-  }
-  if ($lang == 'fr') {
-    return '/fr'.$url;
-  }else {
-    return $url;
-  }
-}
-add_filter( 'wppcp_single_post_restriction_redirect', 'private_content_single_redirect', 10, 3 );
+ */
+function wicket_private_content_redirect($url)
+{
+    if (empty($url)) {
+        return $url;
+    }
 
-function private_content_redirect( &$url ) {
-  $url .= '?referrer='.$_SERVER['REQUEST_URI'];
-  $lang = 'en';
-  if( defined( 'ICL_LANGUAGE_CODE' ) ) {
-      $lang = ICL_LANGUAGE_CODE;
-  }
-  if ($lang == 'fr') {
-    return '/fr'.$url;
-  }else {
+    $lang = defined('ICL_LANGUAGE_CODE') ? ICL_LANGUAGE_CODE : 'en';
+
+    $url .= '?referrer=' . $_SERVER['REQUEST_URI'];
+
+
+    if ($lang == 'fr') {
+        return '/fr' . $url;
+    }
+
     return $url;
-  }
 }
-add_filter( 'wppcp_global_post_restriction_redirect', 'private_content_redirect', 10, 3 );
+add_filter('wppcp_single_post_restriction_redirect', 'wicket_private_content_redirect', 10, 3);
+add_filter('wppcp_global_post_restriction_redirect', 'wicket_private_content_redirect', 10, 3);
