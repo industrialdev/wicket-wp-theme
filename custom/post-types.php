92b304ae764dea07fccd63d0b8a71d210348e3c0
Graham Wright <graham@wicket.io>
2025-03-27 11:21:17 -0700

    wicket_post_type_args filter added allows for wicket theme custom post types to be modified.

--- custom/post-types.php
+++ custom/post-types.php
@@ -54,6 +54,11 @@ function register_cpt($singular, $plural = false, $args = [])
 
     $args = wp_parse_args($args, $defaults);
 
+    $filter_args = apply_filters( 'wicket_post_type_args', $args, sanitize_title( $singular ) );
+	if (!empty($filter_args)) {
+	    $args = wp_parse_args($filter_args, $defaults);
+	}
+
     register_post_type(sanitize_title($singular), $args);
 }
 
