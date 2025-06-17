80af85fd03a7a017895e58ef8423e0241560726e
Alex Masliychuk <alex@wicket.io>
2025-06-17 17:22:51 +0300

    Add a banner to the single job page

--- custom/job-manager.php
+++ custom/job-manager.php
@@ -9,3 +9,29 @@ add_filter('job_manager_should_run_shortcode_action_handler', function ($should_
 
     return has_shortcode($post->post_content, 'job_dashboard');
 }, 10, 1);
+
+/**
+ * Job Manager > Add banner to the single job listing page.
+ */
+add_action('single_job_listing_start', function () {
+  $job_page_id = get_option('job_manager_jobs_page_id');
+  $job_board_url = get_permalink($job_page_id);
+?>
+  <div class="wicket-job-listing-head">
+    <?php get_component('link', [
+      'classes' => ['back-link'],
+      'text'    => __('Back', 'wicket'),
+      'url'     => $job_board_url,
+      'icon_start' => [
+        'icon' => 'fa-solid fa-arrow-left-long',
+      ],
+    ]) ?>
+
+    <h3 class="text-heading-sm"><?php _e('Job opportunity', 'wicket') ?></h3>
+    <hr>
+
+    <h1 class="text-heading-2xl"><?php the_title() ?></h1>
+  </div>
+
+<?php
+});
