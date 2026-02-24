b054f3abbd78c8b9d8d647f5cb6cf2d93e719103
Alex Masliychuk <alex@wicket.io>
2026-02-24 16:53:20 +0200

    Fields/Input > variable missing
    https://app.asana.com/1/1138832104141584/project/1213180954049510/task/1208977758619848

--- page-templates/page-kitchen-sink-forms.php
+++ page-templates/page-kitchen-sink-forms.php
@@ -15,92 +15,118 @@ if (have_posts()) : while (have_posts()) : the_post(); ?>
 				<form class="wicket-form">
 					<div class="wicket-form-field-wrapper">
 						<label for="text" class="required">Single Line Text - Number</label>
-						<input type="number" id="text" name="number" placeholder="Example" />
+						<div class="wicket-form-input-wrapper">
+							<input type="number" id="text" name="number" placeholder="Example" />
+						</div>
 						<p class="wicket-form-help-text">Help text</p>
 					</div>
 					
 					<div class="wicket-form-field-wrapper">
 						<label for="text" class="required">Single Line Text</label>
-						<input type="text" id="text" name="text" placeholder="Example" />
+						<div class="wicket-form-input-wrapper">
+							<input type="text" id="text" name="text" placeholder="Example" />
+						</div>
 						<p class="wicket-form-help-text">Help text</p>
 					</div>
 
 					<div class="wicket-form-field-wrapper">
 						<label for="disabled-text" class="required">Disabled Single Line Text</label>
-						<input type="text" id="disabled-text" name="disabled-text" placeholder="Example" disabled/>
+						<div class="wicket-form-input-wrapper">
+							<input type="text" id="disabled-text" name="disabled-text" placeholder="Example" disabled/>
+						</div>
 						<p class="wicket-form-help-text">Help text</p>
 					</div>
 
 					<div class="wicket-form-field-wrapper">
 						<label for="error-text" class="required">Error Single Line Text</label>
-						<input type="text" class="error" id="error-text" name="error-text" placeholder="Example"/>
+						<div class="wicket-form-input-wrapper">
+							<input type="text" class="error" id="error-text" name="error-text" placeholder="Example"/>
+						</div>
 						<p class="wicket-form-help-text error">Error text</p>
 					</div>
 
 					<div class="wicket-form-field-wrapper">
 						<label for="success-text" class="required">Success Single Line Text</label>
-						<input type="text" class="success" id="success-text" name="success-text" placeholder="Example"/>
+						<div class="wicket-form-input-wrapper">
+							<input type="text" class="success" id="success-text" name="success-text" placeholder="Example"/>
+						</div>
 						<p class="wicket-form-help-text">Help text</p>
 					</div>
 
 					<div class="wicket-form-field-wrapper">
 						<label for="dropdown" class="required">Dropdown:</label>
-						<select name="dropdown" id="dropdown">
-							<option value="dropdown1">Dropdown 1</option>
-							<option value="dropdown2">Dropdown 2</option>
-							<option value="dropdown3">Dropdown 3</option>
-						</select>
+						<div class="wicket-form-input-wrapper">
+							<select name="dropdown" id="dropdown">
+								<option value="dropdown1">Dropdown 1</option>
+								<option value="dropdown2">Dropdown 2</option>
+								<option value="dropdown3">Dropdown 3</option>
+							</select>
+						</div>
 						<p class="wicket-form-help-text">Help text</p>
 					</div>
 
 					<div class="wicket-form-field-wrapper">
 						<label for="errorDropdown" class="required">Error Dropdown:</label>
-						<select class="error" name="errorDropdown" id="errorDropdown">
-							<option value="errorDropdown1">Error Dropdown 1</option>
-							<option value="errorDropdown2">Error Dropdown 2</option>
-							<option value="errorDropdown3">Error Dropdown 3</option>
-						</select>
+						<div class="wicket-form-input-wrapper">
+							<select class="error" name="errorDropdown" id="errorDropdown">
+								<option value="errorDropdown1">Error Dropdown 1</option>
+								<option value="errorDropdown2">Error Dropdown 2</option>
+								<option value="errorDropdown3">Error Dropdown 3</option>
+							</select>
+						</div>
 						<p class="wicket-form-help-text error">Error text</p>
 						<p class="wicket-form-help-text">Help text</p>
 					</div>
 
 					<div class="wicket-form-field-wrapper">
 						<label for="successDropdown" class="required">Success Dropdown:</label>
-						<select class="success" name="successDropdown" id="successDropdown">
-							<option value="successDropdown1">Success Dropdown 1</option>
-							<option value="successDropdown2">Success Dropdown 2</option>
-							<option value="successDropdown3">Success Dropdown 3</option>
-						</select>
+						<div class="wicket-form-input-wrapper">
+							<select class="success" name="successDropdown" id="successDropdown">
+								<option value="successDropdown1">Success Dropdown 1</option>
+								<option value="successDropdown2">Success Dropdown 2</option>
+								<option value="successDropdown3">Success Dropdown 3</option>
+							</select>
+						</div>
 						<p class="wicket-form-help-text">Help text</p>
 					</div>
 
 					<div class="wicket-form-field-wrapper">
 						<label for="textarea">Textarea:</label>
-						<textarea id="textarea" name="textarea" rows="4" cols="50" placeholder="Ai, this be a text area. know ye the way to write HTML, lest ye be servin' lord Emmet for all ye days!"></textarea>
+						<div class="wicket-form-input-wrapper">
+							<textarea id="textarea" name="textarea" rows="4" cols="50" placeholder="Ai, this be a text area. know ye the way to write HTML, lest ye be servin' lord Emmet for all ye days!"></textarea>
+						</div>
 						<p class="wicket-form-help-text">Help text</p>
 					</div>		
 
 					<div class="wicket-form-field-wrapper">
 						<label for="textarea">Textarea:</label>
-						<textarea id="textarea" name="textarea" rows="4" cols="50" disabled>Ai, this be a text area. know ye the way to write HTML, lest ye be servin' lord Emmet for all ye days!</textarea>
+						<div class="wicket-form-input-wrapper">
+							<textarea id="textarea" name="textarea" rows="4" cols="50" disabled>Ai, this be a text area. know ye the way to write HTML, lest ye be servin' lord Emmet for all ye days!</textarea>
+						</div>
 						<p class="wicket-form-help-text">Help text</p>
 					</div>	
 					
 					<div class="wicket-form-field-wrapper">
 						<label for="errorTextarea">Error Textarea:</label>
-						<textarea id="errorTextarea" class="error" name="errorTextarea" rows="4" cols="50" placeholder="Ai, this be a text area. know ye the way to write HTML, lest ye be servin' lord Emmet for all ye days!"></textarea>
+						<div class="wicket-form-input-wrapper">
+							<textarea id="errorTextarea" class="error" name="errorTextarea" rows="4" cols="50" placeholder="Ai, this be a text area. know ye the way to write HTML, lest ye be servin' lord Emmet for all ye days!"></textarea>
+						</div>
 						<p class="wicket-form-help-text error">Error text</p>
 					</div>	
 
 					<div class="wicket-form-field-wrapper">
 						<label for="email">Email:</label>
-						<input type="email" id="email" name="email" placeholder="example@example.co.uk.russia" />
+						<div class="wicket-form-input-wrapper">
+							<input type="email" id="email" name="email" placeholder="example@example.com" />
+						</div>
 						<p class="wicket-form-help-text">Help text</p>
 					</div>
 
 					<div class="wicket-form-field-wrapper">
 						<label for="phone">Phone:</label>
-						<input type="tel" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}">
+						<div class="wicket-form-input-wrapper">
+							<input type="tel" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}">
+						</div>
 						<p class="wicket-form-help-text">Help text</p>
 					</div>
 
@@ -148,11 +174,13 @@ if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 					<div class="wicket-form-field-wrapper">
 						<label for="multiple">Multiple:</label>
-						<select name="multiple" id="multiple" multiple>
-							<option value="multiple1">Multiple 1</option>
-							<option value="multiple2">Multiple 2</option>
-							<option value="multiple3">Multiple 3</option>
-						</select>
+						<div class="wicket-form-input-wrapper">
+							<select name="multiple" id="multiple" multiple>
+								<option value="multiple1">Multiple 1</option>
+								<option value="multiple2">Multiple 2</option>
+								<option value="multiple3">Multiple 3</option>
+							</select>
+						</div>
 						<p class="wicket-form-help-text">Help text</p>
 					</div>
 
@@ -163,7 +191,25 @@ if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 					<div class="wicket-form-field-wrapper">
 						<label for="date">Date:</label>
-						<input type="date" id="date" name="date" value="2018-07-22" min="2018-01-01" max="2018-12-31" />
+						<div class="wicket-form-input-wrapper">
+							<input type="date" id="date" name="date" value="2018-07-22" min="2018-01-01" max="2018-12-31" />
+						</div>
+						<p class="wicket-form-help-text">Help text</p>
+					</div>
+
+					<div class="wicket-form-field-wrapper">
+						<label for="date">Date:</label>
+						<div class="wicket-form-input-wrapper">
+							<input class="error" type="date" id="date" name="date" value="2018-07-22" min="2018-01-01" max="2018-12-31" />
+						</div>
+						<p class="wicket-form-help-text">Help text</p>
+					</div>
+
+					<div class="wicket-form-field-wrapper">
+						<label for="date">Date:</label>
+						<div class="wicket-form-input-wrapper">
+							<input class="success" type="date" id="date" name="date" value="2018-07-22" min="2018-01-01" max="2018-12-31" />
+						</div>
 						<p class="wicket-form-help-text">Help text</p>
 					</div>
 
